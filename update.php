dd17ddf8354f033070a41d2a8963da5becfcc96c
rizalrosyidridho <rosidrizal49@gmail.com>
2022-07-18 21:21:23 +0700

    menyimpan responsi pweb

---
+++ update.php
@@ -0,0 +1,62 @@
+<?php
+include 'functions.php';
+$pdo = pdo_connect_mysql();
+$msg = '';
+// Check if the contact id exists, for example update.php?id=1 will get the contact with the id of 1
+if (isset($_GET['id'])) {
+    if (!empty($_POST)) {
+        // This part is similar to the create.php, but instead we update a record and not insert
+        $id = isset($_POST['id']) ? $_POST['id'] : NULL;
+        $nama = isset($_POST['nama']) ? $_POST['nama'] : '';
+        $email = isset($_POST['email']) ? $_POST['email'] : '';
+        $notelp = isset($_POST['notelp']) ? $_POST['notelp'] : '';
+        $jenis = isset($_POST['jenis']) ? $_POST['jenis'] : '';
+        
+        // Update the record
+        $stmt = $pdo->prepare('UPDATE data SET id = ?, nama = ?, email = ?, notelp = ?, jenis = ? WHERE id = ?');
+        $stmt->execute([$id, $nama, $email, $notelp, $jenis, $_GET['id']]);
+        $msg = 'Updated Successfully!';
+    }
+    // Get the contact from the contacts table
+    $stmt = $pdo->prepare('SELECT * FROM data WHERE id = ?');
+    $stmt->execute([$_GET['id']]);
+    $contact = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$contact) {
+        exit('Contact doesn\'t exist with that ID!');
+    }
+} else {
+    exit('No ID specified!');
+}
+?>
+
+
+
+<?=template_header('Read')?>
+
+<div class="content update">
+	<h2>Update Contact #<?=$contact['id']?></h2>
+    <form action="update.php?id=<?=$contact['id']?>" method="post">
+        <label for="id">ID</label>
+        <label for="nama">Nama</label>
+        <input type="text" name="id" value="<?=$contact['id']?>" id="id">
+        <input type="text" name="nama" value="<?=$contact['nama']?>" id="nama">
+        <label for="email">Email</label>
+        <label for="notelp">No. Telp</label>
+        <input type="text" name="email" value="<?=$contact['email']?>" id="email">
+        <input type="text" name="notelp" value="<?=$contact['notelp']?>" id="notelp">
+        <label for="jenis">jenis</label>
+        <select class="" name="jenis">
+        <option value="Civic">Civic</option>
+        <option value="BMW">Avanza</option>
+        <option value="Veloz">Xenia</option>
+        <option value="Inova">Inova</option>
+        <option value="Jazz">Jazz</option>
+        </select>
+        <input type="submit" value="Update">
+    </form>
+    <?php if ($msg): ?>
+    <p><?=$msg?></p>
+    <?php endif; ?>
+</div>
+
+<?=template_footer()?>
